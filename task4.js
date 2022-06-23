c801727bca9dd122876d20668dc8f61b086cb92b
Fhmi00 <anisfahmi045@gmail.com>
2022-06-23 09:10:27 +0700

    rev

--- task4.js
+++ task4.js
@@ -2,5 +2,8 @@ const fetch = require('node-fetch')
 
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(res => res.json())
-.then(data => )
+.then(data => data.map(x => (console.log(x['name']))))
+
+
+
     
